26753fa148e15355a1f7ff328d56130f11a5a01d
Theresa Whynot <132946455+theresa-whynot@users.noreply.github.com>
2023-10-13 09:38:46 -0400

    Update topsegment.js

--- src/components/topsegment.js
+++ src/components/topsegment.js
@@ -19,7 +19,7 @@ const TopSegment = () => {
                 <h2>Theresa Whynot - Web & Blockchain Developer</h2>
                 <p>
                     After 6 years spent in Software Consulting at IBM - working with clients such as Google,
-                    Johson&Johnson, Wal-Mart and New Balance- I am ready for a new adventure in the
+                    Johson&Johnson, Wal-Mart and New Balance- I wanted a new adventure in the
                     software engineering world. With a year spent independently mastering web development
                     and blockchain fundamentals, I've honed a skill set that's fueled by an unwavering
                     enthusiasm for programming and blockchain technology. 
